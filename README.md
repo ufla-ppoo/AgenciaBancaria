22d85bcfb9b7613de23c404bb43152d38d8dd00f
Julio César Alves <julio.caburu@gmail.com>
2022-08-03 14:23:09 -0300

    Versão inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+## Projeto Agência Bancária
+
+Projeto utilizado como exemplo nas aulas de Tratamentos de Exceção.
---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        InterfaceAgencia interfAgencia = new InterfaceAgencia("Agência UFLA");
+        interfAgencia.exibir();
+    }
+}
